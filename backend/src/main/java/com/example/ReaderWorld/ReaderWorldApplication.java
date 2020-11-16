16014532ed09279ffede34aad4333761269c96fb
kaanapan <e2236925@ceng.metu.edu.tr>
2020-11-16 09:57:23 +0300

    Backend initialization

---
+++ backend/src/main/java/com/example/ReaderWorld/ReaderWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReaderWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderWorldApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ReaderWorld/ReaderWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReaderWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
