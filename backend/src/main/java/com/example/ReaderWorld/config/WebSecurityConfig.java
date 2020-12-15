57c9595164c1f576a65c9bae7e1c5405e3586bb0
kaanapan <e2236925@ceng.metu.edu.tr>
2020-12-15 17:07:39 +0300

    Cors configuration

--- backend/src/main/java/com/example/ReaderWorld/config/WebSecurityConfig.java
+++ backend/src/main/java/com/example/ReaderWorld/config/WebSecurityConfig.java
@@ -17,7 +17,12 @@ import org.springframework.security.web.authentication.logout.HttpStatusReturnin
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
+import java.util.Arrays;
 
 import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
@@ -74,4 +79,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE");
+            }
+        };
+    }
 }
