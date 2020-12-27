fb7df41b764986f246bd9c1e948143da1a7f6e7a
kaanapan <e2236925@ceng.metu.edu.tr>
2020-12-27 22:37:27 +0300

    Cors Update

--- backend/src/main/java/com/example/ReaderWorld/config/WebSecurityConfig.java
+++ backend/src/main/java/com/example/ReaderWorld/config/WebSecurityConfig.java
@@ -82,7 +82,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(ImmutableList.of("http://localhost:3000/", "http://localhost:8080"));
+        configuration.setAllowedOrigins(ImmutableList.of("http://localhost:3000", "http://localhost:8080"));
         configuration.setAllowedMethods(ImmutableList.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type", "Cookie", "Referer", "User-Agent", "Set-Cookie"));
