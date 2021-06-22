5f16c1f523a2b7507ed83f5467f950b82b34d985
Burcu Aykan <burcuaykan13@gmail.com>
2021-06-22 17:58:05 +0300

    signup bug fixing-15

--- frontend/src/components/pages/welcome/welcome-page.js
+++ frontend/src/components/pages/welcome/welcome-page.js
@@ -1,18 +1,64 @@
 import React from 'react'
 import './welcome-page.css';
 import SignIn from './sign-in/sign-in';
+import React, { Component } from "react";
+import axios from 'axios';
+import { Redirect } from "react-router-dom";
+
+
+export default class WelcomePage extends Component {
+  constructor(props) {
+    super(props)
 
 
 
-const WelcomePage = () => {
-
-    return (
-      <div id="welcome-page">
-        <div >
-          <SignIn/>
-        </div>
-      </div>
-    );
+    this.state = {
+      username: '',
+      password: '',
+      loggedIn: false,
+      signUp: false
+    }
   }
-  
-  export default WelcomePage;
+  async componentDidMount() {
+    const userObject = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    var querystring = require('querystring');
+    await axios.post('http://readerworld.ceng.metu.edu.tr:8080/login',
+      // userObject,
+      querystring.stringify({
+        username: "ceng492@user.com", //gave the values directly for testing
+        password: "ceng492",
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        withCredentials: true
+      })
+      .then((response) => {
+        this.setState({ loggedIn: true });
+        console.log(response);
+      }).catch((error) => {
+        console.log(error)
+      });
+  }
+  render() {
+    if (this.state.loggedIn) {
+      return <Redirect to='/main-page' />
+    }
+  }
+
+
+  /*return (
+    <div id="welcome-page">
+      <div >
+        <SignIn/>
+      </div>
+    </div>
+  );*/
+}
+
+export default WelcomePage;
